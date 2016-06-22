d3d093441e91b3ba3bcd75829b46627a1476655b
Ricardo Rossi <rrossi@endata.com>
2016-06-21 19:33:05 -0500

    major change

--- server/run.go
+++ server/run.go
@@ -15,7 +15,7 @@ func main() {
 	fmt.Println("Starting server on http://" + host)
 	router := web.New(context{})
 	router.Get("/", func(rw web.ResponseWriter, req *web.Request) {
-		fmt.Fprint(rw, "<h1>Hello Meetup!</h1>")
+		fmt.Fprint(rw, "<h1>Hello Meetup!!!!</h1>")
 	})
 	http.ListenAndServe(host, router)
 }
